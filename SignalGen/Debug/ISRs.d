6eb916139156eedf1b5810c249cd48f237c4d742
AlexPurr <almperr@iupuc.edu>
2018-06-25 11:17:19 -0400

    Initial Upload

---
+++ SignalGen/Debug/ISRs.d
@@ -0,0 +1,19 @@
+# FIXED
+
+ISRs.obj: ../ISRs.c
+ISRs.obj: ../DSP_Config.h
+ISRs.obj: ../LCDK_Support_DSP.h
+ISRs.obj: ../tistdtypes.h
+ISRs.obj: ../OMAPL138_defines.h
+ISRs.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.3/include/c6x.h
+ISRs.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.3/include/vect.h
+ISRs.obj: C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.3/include/stdint.h
+
+../ISRs.c: 
+../DSP_Config.h: 
+../LCDK_Support_DSP.h: 
+../tistdtypes.h: 
+../OMAPL138_defines.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.3/include/c6x.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.3/include/vect.h: 
+C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.3/include/stdint.h: 
---
+++ SignalGen/Debug/subdir_rules.mk
@@ -0,0 +1,43 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+ISRs.obj: ../ISRs.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.3/bin/cl6x" -mv6740 --include_path="C:/Users/Alexander/Documents/ECET490/TI_Programming/SignalGen" --include_path="C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.3/include" -g --diag_warning=225 --diag_wrap=off --display_error_number --preproc_with_compile --preproc_dependency="ISRs.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+LCDK_Support_DSP.obj: ../LCDK_Support_DSP.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.3/bin/cl6x" -mv6740 --include_path="C:/Users/Alexander/Documents/ECET490/TI_Programming/SignalGen" --include_path="C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.3/include" -g --diag_warning=225 --diag_wrap=off --display_error_number --preproc_with_compile --preproc_dependency="LCDK_Support_DSP.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+StartUp.obj: ../StartUp.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.3/bin/cl6x" -mv6740 --include_path="C:/Users/Alexander/Documents/ECET490/TI_Programming/SignalGen" --include_path="C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.3/include" -g --diag_warning=225 --diag_wrap=off --display_error_number --preproc_with_compile --preproc_dependency="StartUp.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+main.obj: ../main.c $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.3/bin/cl6x" -mv6740 --include_path="C:/Users/Alexander/Documents/ECET490/TI_Programming/SignalGen" --include_path="C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.3/include" -g --diag_warning=225 --diag_wrap=off --display_error_number --preproc_with_compile --preproc_dependency="main.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+vectors.obj: ../vectors.asm $(GEN_OPTS) | $(GEN_HDRS)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C6000 Compiler'
+	"C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.3/bin/cl6x" -mv6740 --include_path="C:/Users/Alexander/Documents/ECET490/TI_Programming/SignalGen" --include_path="C:/ti/ccsv8/tools/compiler/ti-cgt-c6000_8.2.3/include" -g --diag_warning=225 --diag_wrap=off --display_error_number --preproc_with_compile --preproc_dependency="vectors.d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
