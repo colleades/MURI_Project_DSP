5bb9a7cc92b427e43cae94718bd2982060eb9c85
colleades <40572507+colleades@users.noreply.github.com>
2018-07-17 12:56:30 -0400

    now includes serial communication

--- SerialPlaybackTest/main.c
+++ SerialPlaybackTest/main.c
@@ -11,22 +11,40 @@
 #include "DSP_Config.h"   
 
 int main()
-{    
-	
-	// initialize DSP board
-  	DSP_Init();
+  {
 
-	// call StartUp for application specific code
-	// defined in each application directory
-	StartUp();
-	
-	// main stalls here, interrupts drive operation 
-  	while(1) { 
-		
-		//random test function, delete later
-		helloThere();
-		;
-  	}   
+    // initialize DSP board
+    DSP_Init();
+    Init_UART2(115200);
+
+    // call StartUp for application specific code
+    // defined in each application directory
+    StartUp();
+
+    // main stalls here, interrupts drive operation
+    while(1)
+    {
+        Uint8 dataReady = IsDataReady_UART2();
+
+        if(dataReady != 0)
+        {
+           Uint8 serialTest = Read_UART2();
+           printf("%u", serialTest);
+           puts("\n");
+         
+           //change unsigned to int
+           //call function defined in ISR and pass it serialTest
+           int intSerialTest = (int)(serialTest);
+           helloThere (intSerialTest);
+         
+         
+           //puts("Cats");
+
+        }
+
+    }
+
+
 }
 
 
