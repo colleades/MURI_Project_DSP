93dec370f4dff41d101c519ef7bf5e871c6d4f0c
colleades <40572507+colleades@users.noreply.github.com>
2018-07-18 12:34:58 -0400

    Basic Structure

--- Skeleton_Structure/main.c
+++ Skeleton_Structure/main.c
@@ -1,1 +1,46 @@
 //this is the main
+
+
+int main()
+{
+  
+  
+  //Initialize DSP board and call StartUp() function here
+  
+  //while loop
+   while(1)
+    {
+        Uint8 dataReady = IsDataReady_UART2();
+
+        if(dataReady != 0)
+        {
+           Uint8 serialTest = Read_UART2();
+           
+           //cast unsigned to int
+           int intSerialTest = (int)(serialTest);
+          
+           //select which function is called in ISR
+           if (intSerialTest == 100){
+             
+             tapTempo();
+           
+           }elseif (intSerialTest !=0 && <4){
+             
+             //call beat repeat function and pass the specific beat repeat
+             setBeatRepeat(intSerialTest);
+             
+           }elseif (intSerialTest < 28 && > 4){
+             
+             //call time travel and pass specific time travel 
+             setTimeTravel(intSerialTest);
+             
+           }else{
+             
+             //if no recognizable serial number is inputted, continue dry playback
+             activeSerialButton == 0;
+               
+           }
+     
+        }
+
+    }
