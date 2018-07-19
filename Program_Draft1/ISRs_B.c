378887bec4bea2c0b4101623f7494f3dcc5f7f53
colleades <40572507+colleades@users.noreply.github.com>
2018-07-19 12:30:57 -0400

    Update ISRs_B.c

--- Program_Draft1/ISRs_B.c
+++ Program_Draft1/ISRs_B.c
@@ -27,23 +27,35 @@ volatile union {
 	Int16 Channel[2];
 } CodecDataIn, CodecDataOut;
 
-/* add any global variables here */
+
+//GLOBAL VARIABLES
+//**********************************************
+//Recording
+int recIndex = 0;
+int bufferEnd = (24000*8)-1;//default to 120 bpm
 float xLeft, xRight, yLeft, yRight;
-Uint32 recIndex = 0; // index for buffer value
-float playbackIndex = 0; 
-
-//Collin edits
-
-
-
-//Original
-float playbackSpeed = 1.0;
-//variable for what current serial info is being sent
-int serialValue = 0;
-#define BUFFER_LENGTH   96000 // buffer length in samples
+#define BUFFER_LENGTH   1152000 // buffer length in samples
 #pragma DATA_SECTION (buffer, "CE0"); // put "buffer" in SDRAM
 volatile float buffer[2][BUFFER_LENGTH]; // space for left + right
-volatile float gain = 0.75; /* set gain value for echoed sample */
+
+//Wet Playback
+int activeButton = 0;
+int playbackIndex = 0;
+
+//Time Travel
+float playbackSpeed = 1;
+float startSpeed = 1;
+float endSpeed = 1;
+int glideCounter = 0;
+
+//BeatRepeat
+int loopStart = 0;
+int loopEnd = 24000;
+
+//Tap Tempo
+int sampleCounter = 144000;
+int oneBeat = 24000;
+
 
 void ZeroBuffer()
 ///////////////////////////////////////////////////////////////////////
@@ -67,32 +79,129 @@ void ZeroBuffer()
 }
 
 
-
-
-
-
-
-
-/************RANDOM FUNCTION THAT'S STUPID*********/////////
-
-void helloThere (int intSerialTest) {
-	
-	//set ISR serial value to whatever number is sent in from main
-	serialValue = intSerialTest;
-	
-	/*puts("\n");
-	printf("This is from the ISR file: %d", intSerialTest);
-	puts("\n");*/
-		
+//SET ACTIVE BUTTON
+//*******************************
+void setActiveButton(int newButtonValue)
+{
+	activeButton = newButtonValue;
 }
 
-/****************************************************///////
 
+//TAP TEMPO
+//*********
+void tapTempo(){
+	
+          
+  	//if sampleCounter is less than maximum beat size (20 samples), set oneBeat to current number of samples
+ 	if (sampleCounter < 144000){
+		
+           	//1beat
+          	oneBeat = sampleCounter;
+            
+           	//set buffer size
+           	bufferEnd = ((oneBeat*8)-1);
+		 
+	 }
+	
+	 sampleCounter = 0;
+}
+         
+//SET BEAT REPEAT
+//***************
+void setBeatRepeat(int serialNumber){
+	
+        setActiveButton(1);
+		
+		int beatDivision = oneBeat/serialNumber;
+	
+	//set playback index to current record spot
+	playbackIndex = recIndex;
+          
+        //set loop start and end points
+        loopStart = playbackIndex;
+        loopEnd = ((playbackIndex+beatDivision)%bufferEnd);
+                   
+}
+         
+//TIME TRAVEL
+//***********
+void setTimeTravel(int serialNumber){
+	
+	float newPlaybackSpeed = serialToPlaybackSpeed(serialNumber);
+	
+	if (activeButton == 0)//pressing to jump to a speed
+	{
+        setActiveButton(2);
+		playbackSpeed = newPlaybackSpeed;
+		endSpeed = newPlaybackSpeed;
+	}
+	else if (activeButton == 2)//when dragging finger to different speeds
+	{
+		if(playbackSpeed == endSpeed)//initiate glide when starting from a stationary speed
+		{
+			glideCounter == 0;
+			startSpeed = playbackSpeed;
+		
+		}
+		endSpeed = newPlaybackSpeed;
+	}
+	
+	//set playback to current recording spot
+	playbackIndex = recIndex;
+	
+}
 
-
-
-
-
+float serialToPlaybackSpeed(int serialNumber)
+{
+	 //change playback speed
+        if (serialNumber == 20){
+		
+		//-2
+            	return -2;
+            
+        }else if (serialNumber == 21){
+		
+            
+          	//-1
+            	return -1;
+          
+            
+        }else if (serialNumber == 22){
+		
+		//-0.5
+           	return -0.5;
+            
+        }else if (serialNumber == 23){
+	
+            	//tape stoppppppppp (0)
+            	return 0;
+         
+        }else if (serialNumber == 24){
+		
+	        //0.5
+            	return 0.5;
+            
+        }else if (serialNumber == 25){
+            
+            	//1
+           	return 1
+            
+        }else if (serialNumber == 26){
+            
+            	//2
+            	return 2
+            
+        }else if (serialNumber == 27){
+            
+            	//4
+            	return 4;
+            
+        }else{
+            
+            	return 0;
+            
+        }
+}
 
 
 interrupt void Codec_ISR()
@@ -116,89 +225,107 @@ interrupt void Codec_ISR()
 
   	CodecDataIn.UINT = ReadCodecData();		// get input data samples
 	
-	/* add your code starting here */
-
-	/****************************
-	ECHO ROUTINE BEGINS HERE
-	****************************/
-	/*xLeft = CodecDataIn.Channel[LEFT];   // current LEFT input value to float
-	xRight = CodecDataIn.Channel[RIGHT];   // current RIGHT input value to float
-
-	buffer[LEFT][recIndex] = xLeft;
-	buffer[RIGHT][recIndex] = xRight;
-	newest = oldest;
-	if (++oldest >= BUFFER_LENGTH) // implement circular buffer
-		oldest = 0;
-
-	// use either FIR or IIR lines below
-
-	// for FIR comb filter effect, uncomment next two lines
-	yLeft = xLeft + (gain * buffer[LEFT][oldest]);
-	yRight = xRight + (gain * buffer[RIGHT][oldest]);*/
-    
-	// for IIR comb filter effect, uncomment four lines below
-	//buffer[LEFT][newest] = xLeft + (gain * buffer[LEFT][oldest]);
-	//buffer[RIGHT][newest] = xRight + (gain * buffer[RIGHT][oldest]);
-	//yLeft = buffer[LEFT][oldest];  // or use newest 
-	//yRight = buffer[RIGHT][oldest];  // or use newest
 	
-	//RECORD TO BUFFER
-	//~~~~~~~~~~~~~~~~~~~~~	
+	recordCurrentSample();
+	incTapTempoCounter();
+	
+	if(activeButton == 0)//No Buttons Pressed
+	}
+		dryPlayback();
+	}
+	else if(activeButton == 1)//BeatRepeat Button Pressed
+	}
+		beatRepeat();
+	}
+	else if(activeButton == 2)//PlaybackSlider Pressed
+	}
+		playbackSpeed();
+	}
+	
+
+	WriteCodecData(CodecDataOut.UINT);		// send output data to  port
+
+}//end ISR
+
+
+//RECORD CURRENT SAMPLE
+//************************
+void recordCurrentSample()
+{
 	buffer[LEFT][recIndex] = CodecDataIn.Channel[LEFT];
 	buffer[RIGHT][recIndex] = CodecDataIn.Channel[RIGHT];
 	
-	if (++recIndex >= BUFFER_LENGTH) // implement circular buffer recording
+	if (++recIndex >= bufferEnd) // implement circular buffer
 		recIndex = 0;
-	//~~~~~~~~~~~~~~~~~~~~~
-	//end RECORD TO BUFFER	
-	
-	//PLAYBACK SPEED TEST
-	//~~~~~~~~~~~~~~~~~~~~~	
-	//int roundedPlaybackIndex = (int) round(playbackIndex);
-	
-	if (playbackIndex >= BUFFER_LENGTH) // Forward and Backward circular playback
+}//end record current sample
+
+
+//INCREMENT TAP TEMPO COUNTER CURRENT SAMPLE
+//************************
+void incTapTempoCounter()
+{
+	if(sampleCounter != 144000)
 	{
-		playbackIndex = 0;
+		sampleCounter++;
+	}
+}//end incTapTempoCounter
+
+
+//DRY PLAYBACK
+//************************
+void dryPlayback()
+{
+	CodecDataOut.Channel[LEFT] = CodecDataIn.Channel[LEFT];
+	CodecDataOut.Channel[RIGHT] = CodecDataIn.Channel[RIGHT];
+}//end dry playbackIndex
+
+
+//BEAT REPEAT
+//************************
+void beatRepeat()
+{
+	//output buffer audio
+	int intPlaybackIndex = (int)(playbackIndex);
+	
+	CodecDataOut.Channel[LEFT] = buffer[LEFT][intPlaybackIndex];
+	CodecDataOut.Channel[RIGHT] = buffer[RIGHT][intPlaybackIndex];
+	
+	playbackIndex++;
+	
+	if(playbackIndex == (loopEnd + 1))//loop points
+		playbackIndex = loopStart;
+	
+	if (playbackIndex >= bufferEnd) //wrap around buffer
+		playbackIndex = 0;		
+	
+}//end beat repeat
+
+//PLAYBACK SPEED
+//************************
+void playbackSpeed()
+{
+	if (playbackIndex > bufferEnd) // Forward and Backward circular playback
+	{
+		playbackIndex = (playbackIndex - bufferEnd - 1);
 	}
 	else if (playbackIndex < 0)
 	{
-		playbackIndex = (BUFFER_LENGTH - 1);
+		playbackIndex = (bufferEnd + playbackIndex + 1);
 	}
 	
 	int intPlaybackIndex = (int)(playbackIndex);
 	
-	//PLAYBACK SPEED TEST!!!!!!!!!
+	CodecDataOut.Channel[LEFT] = buffer[LEFT][intPlaybackIndex];
+	CodecDataOut.Channel[RIGHT] = buffer[RIGHT][intPlaybackIndex];
 	
-	//if serialTest is 24, playbackspeed is .5, otherwise, it's 1
-	if (serialValue == 24){
-		
-		playbackSpeed = 0.5;
-	else
-		playbackSpeed = 1;
-		
-		
+	if(playbackSpeed != endSpeed)//glide playback speed
+	{
+		playbackSpeed = ((endSpeed - startSpeed)*(glideCounter/oneBeat)+startSpeed);
+		glideCounter++;
 	}
-	yLeft = buffer[LEFT][intPlaybackIndex];
-	yRight = buffer[RIGHT][intPlaybackIndex];
 	
-	playbackIndex = (playbackIndex + playbackSpeed);
-	
-	/*int testIndex = 1231;
-	
-	yLeft = buffer[LEFT][roundedPlaybackIndex];	
-	yRight = buffer[RIGHT][roundedPlaybackIndex];
-	
-	//printf("The left sample: %f\n The right sample: %f\n", yLeft, yRight);
-	
-	//exit(0);
-	
-	int newIndex = recIndex - 1;*/
+	//get the new playback index
+	playbackIndex = playbackIndex + playbackSpeed;
+}//end playback speed
 
-	CodecDataOut.Channel[LEFT] = yLeft;   // output the LEFT value
-	CodecDataOut.Channel[RIGHT] = yRight; // output the RIGHT value
-	/*****************************/
-	/* end your code here */
-
-	WriteCodecData(CodecDataOut.UINT);		// send output data to  port
-}
 
